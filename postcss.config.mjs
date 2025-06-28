cb5bba98c46b2e8880441a70282d00623a09a437
Ananya Singh <78841020+ananya-singhB@users.noreply.github.com>
2025-06-28 19:46:08 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+const configs = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
 };
 
-export default config;
+export default configs;
